06522cda098dc1ddde2299758d6e3ac09a0ab8ce
Oscar Arnaiz <oscar@appointedd.com>
2017-06-26 23:06:19 +0100

    Housekeeping. Removing js files

--- app/main/player.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Player = (function () {
-    function Player() {
-    }
-    return Player;
-}());
-exports.Player = Player;
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoicGxheWVyLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsicGxheWVyLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7O0FBQUE7SUFBQTtJQVNBLENBQUM7SUFBRCxhQUFDO0FBQUQsQ0FBQyxBQVRELElBU0M7QUFUWSx3QkFBTSIsInNvdXJjZXNDb250ZW50IjpbImV4cG9ydCBjbGFzcyBQbGF5ZXIge1xuICAgIGlkOiBudW1iZXI7XG4gICAgZmlyc3RfbmFtZTogc3RyaW5nO1xuICAgIGZwcGc6IG51bWJlcjtcbiAgICBpbWFnZXM6IHtcbiAgICAgICAgZGVmYXVsdDp7XG4gICAgICAgICAgICB1cmw6c3RyaW5nO1xuICAgICAgICB9XG4gICAgfVxufVxuIl19
